77ed159a4ff6d40152baf75fcf915fb45acafd8b
Wang-Phil <1420767429@qq.com>
2024-04-18 13:44:29 +0800

    Leetcode53，做过很多遍的题目，再次看到的时候有点思路，但是并没有想到是动态规划，真的是太笨了。

---
+++ src/leetcode/hot100/Leetcode53.java
@@ -0,0 +1,29 @@
+package leetcode.hot100;
+/*
+ *Author: [WangWeicheng]
+ *Date: 2024/4/18
+ *Time: 12:39
+ * 给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+子数组
+是数组中的一个连续部分。
+ */
+
+import java.util.Arrays;
+
+//最大子数组之和
+public class Leetcode53 {
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        //动态规划dp[i] = dp[i-1],dp[i-1]+nums[i]
+        int pre = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            pre = Math.max(nums[i], pre + nums[i]);
+            max = Math.max(max, pre);
+        }
+        return max;
+    }
+}
