88adfe2e5da2aa324e0fc4a3eac5c01ca2465e05
Wang-Phil <1420767429@qq.com>
2024-07-04 10:09:13 +0800

    今天主要做ppt

---
+++ src/leetcode/krahets/Leetcode136.java
@@ -0,0 +1,22 @@
+package leetcode.krahets;
+/*
+ *@version 1.0
+ *@Author: WangWeicheng
+ *@Date: 2024/7/4
+ *@Time: 9:53
+ * 给你一个 非空 整数数组 nums ，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+你必须设计并实现线性时间复杂度的算法来解决此问题，且该算法只使用常量额外空间。
+ */
+
+public class Leetcode136 {
+    public static void main(String[] args) {}
+    //只出现一次的数字
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
